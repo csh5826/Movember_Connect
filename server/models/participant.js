99326e38f2d300cc23b1178e64bdf157237f12a7
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-27 12:34:29 -0400

    changed around participant schema

--- server/models/participant.js
+++ server/models/participant.js
@@ -3,19 +3,21 @@ const Schema = mongoose.Schema
 
 const ParticipantSchema = new Schema({
     name: String,
-    latitude: Number,
-    longitude: Number,
+    location: {
+        type: {type: String, default: 'Point'},
+        coordinates: {type: [Number], default:[0, 0], required: true}
+    },
     cause: String,
     pledge: Number, 
     story: String
 
 })
 
+// geoJSON in mongoose, make sure you put LONGITUDE first, then LATITUDE
+
 module.exports = mongoose.model('Participant', ParticipantSchema)
 
-//{"_id":{"$oid":"5f9765730f85c0342b30809c"},
 //"name":"Franco","latitude":"35.991947",
 //"longitude":"-78.903871",
 //"pledge":49047,
 //"story":"Alias minus quia et. Est perspiciatis animi beatae. Ea porro qui cupiditate iste dolore porro rerum. Ut aperiam et facere. Quos dicta perspiciatis et at expedita delectus enim. Totam deserunt reiciendis provident ratione id optio harum cumque non."
-//,"__v":0}
--- server/routes/main.js
+++ server/routes/main.js
@@ -3,6 +3,17 @@ const { response } = require('express')
 const faker = require('faker')
 const Participant = require('../models/participant')
 
+// Participant.create({
+//     name: 'Joe Dirt',
+//     location: {
+//         type: 'Point',
+//         coordinates: [-78.455555, 37.111432]
+//     },
+//     cause: "Testicular cancer",
+//     pledge: 165,
+//     story:"Alias minus quia et. Est perspiciatis animi beatae. Ea porro qui cupiditate iste dolore porro rerum. Ut aperiam et facere. Quos dicta perspiciatis et at expedita delectus enim. Totam deserunt reiciendis provident ratione id optio harum cumque non."
+// })
+
 // generates fake data for participants
 router.get('/generate-fake-data', (req, res, next) => {
     for (let i = 0; i < 5; i++) {
@@ -20,7 +31,7 @@ router.get('/generate-fake-data', (req, res, next) => {
   })
 
 //TODO 
-// filter based on pledge > 100 and < 100 
+// figure out how to query based on certain coordinate locations
 
 router.get('/participants', (req,res, next) => {
     //optional queries to filter participants by
@@ -39,12 +50,11 @@ router.get('/participants', (req,res, next) => {
         }
         if (pledge === 'low'){
             x = {$lt:100}
-            // works :  {pledge: {$lt:100}}
         }
     }
     // if no pledge param, make sure all participants are grabbed
     if (!pledge) {
-            x = {$gt:0}
+            x = {$gte:0}
     }
     Participant
     .find(query)
