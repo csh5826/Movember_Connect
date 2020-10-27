3eaa8509a6a5499e311628c772be7d11f5d13b26
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-26 21:34:54 -0400

    adds a get route for individual participants and a post route to create a new one

--- server/models/participant.js
+++ server/models/participant.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema
 
 const ParticipantSchema = new Schema({
     name: String,
-    longitude: Number,
     latitude: Number,
+    longitude: Number,
     cause: String,
     pledge: Number, 
     story: String
@@ -12,3 +12,10 @@ const ParticipantSchema = new Schema({
 })
 
 module.exports = mongoose.model('Participant', ParticipantSchema)
+
+//{"_id":{"$oid":"5f9765730f85c0342b30809c"},
+//"name":"Franco","latitude":"35.991947",
+//"longitude":"-78.903871",
+//"pledge":49047,
+//"story":"Alias minus quia et. Est perspiciatis animi beatae. Ea porro qui cupiditate iste dolore porro rerum. Ut aperiam et facere. Quos dicta perspiciatis et at expedita delectus enim. Totam deserunt reiciendis provident ratione id optio harum cumque non."
+//,"__v":0}
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { response } = require('express')
 const faker = require('faker')
+const participant = require('../models/participant')
 const Participant = require('../models/participant')
 
 router.get('/generate-fake-data', (req, res, next) => {
@@ -8,7 +9,7 @@ router.get('/generate-fake-data', (req, res, next) => {
       let participant = new Participant()
   
       participant.name = faker.name.firstName()
-      participant.pledge = faker.random.number()
+      participant.pledge = faker.commerce.price()
       participant.story = faker.lorem.sentences()
   
       participant.save((err) => {
@@ -17,5 +18,31 @@ router.get('/generate-fake-data', (req, res, next) => {
     }
     res.end()
   })
+
+// get the list of all participants
+router.get('/participants', (req,res, next) => {
+
+})
+
+// create a new participant in the db
+router.post('/participants', (req,res, next) => {
+    let participant = new Participant(req.body);
+    participant.save()
+    res.send(participant)
+})
+
+// get participant by id
+  router.get('/participants/:participant', (req, res, next) => {
+    let participantId = req.params.participant
+
+  Participant
+    .findById(participantId).exec((err, participant) => {
+        if (err || participant._id === null) {
+            res.sendStatus(400)
+        } else {
+            res.send(participant);
+        }
+    })
+})
 
   module.exports = router
