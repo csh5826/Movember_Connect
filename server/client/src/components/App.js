7729ae64a793332d329685f6945ee629bb21751b
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-27 17:53:12 -0400

    rearranged index.js and app.js

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
+import Participant from './Participant';
+import NewParticipant from './NewParticipant';
+import Main from './Main'
 const App = () => {
   return (
-    <div>Hi
-    </div>
+    <Fragment>
+      <Switch>
+          <Route exact path='/' component={Main}/>
+          <Route exact path='/participants/new' component={NewParticipant}/>
+          <Route path='/participants/:id' component={Participant}/>
+        </Switch>
+    </Fragment>
   );
 }
 
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,13 +2,11 @@ import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import reducers from './reducers';
 // import promise from 'redux-promise';
 
 import App from './components/App';
-import Participant from './components/Participant';
-import NewParticipant from './components/NewParticipant'
 
 // const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,11 +15,7 @@ ReactDOM.render(
   // <Provider>
     <BrowserRouter>
       <Fragment>
-        <Switch>
-          <Route exact path='/' component={App}/>
-          <Route exact path='/participants/new' component={NewParticipant}/>
-          <Route path='/participants/:id' component={Participant}/>
-        </Switch>
+        <App />
       </Fragment>
     </BrowserRouter>
   // </Provider>,
