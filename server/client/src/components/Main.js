93ca0c60788e85b119cc2ef651b8814b04c0989d
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-27 20:21:23 -0400

    adds initial maps, needs styling

--- server/client/src/components/Main.js
+++ server/client/src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import NavBar from './NavBar';
+import Map from './Map';
 
 const Main = () => {
   return (
     <Fragment>
         <NavBar />
+        <Map />
     </Fragment>
   );
 }
---
+++ server/client/src/components/Map.js
@@ -0,0 +1,53 @@
+import React, { Fragment, Component } from 'react';
+import mapboxgl from 'mapbox-gl';
+import Container from '@material-ui/core/Container';
+
+mapboxgl.accessToken =
+    'pk.eyJ1IjoiY3NoNTgyNiIsImEiOiJja2Y4ODRnbm0wNmRmMnlvMzJsZHllYWNmIn0.ahh2fZ9MyzBjG2ZAmfRzoQ';
+
+class Map extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lng: -78.9032316,
+            lat: 35.9962091,
+            zoom: 14
+        };
+    }
+    //render the map on load of page
+    componentDidMount() {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom,
+        });
+        //changes the lng and lat to scroll correctly
+        map.on('move', () => {
+            this.setState({
+                lng: map.getCenter().lng.toFixed(4),
+                lat: map.getCenter().lat.toFixed(4),
+                zoom: map.getZoom().toFixed(2)
+            });
+        });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Container>
+                <div className='col'>
+                    <div>
+                        Longitude: {this.state.lng} | Latitude: {this.state.lat} |
+                        Zoom: {this.state.zoom}
+                     </div>
+                        <div ref={(el) => (this.mapContainer = el)} className='mapContainer'/>
+                    </div>
+                </Container>
+            </Fragment>
+        )
+    }
+}
+
+export default Map;
