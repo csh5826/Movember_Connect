ba2cccf69bc9a27d1494e693646fe881337bb3b8
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-10-28 09:03:49 -0400

    more styling for form

--- server/client/src/components/NewParticipant.js
+++ server/client/src/components/NewParticipant.js
@@ -2,7 +2,10 @@ import React, { Fragment } from 'react';
 import NavBar from './NavBar';
 import TextField from '@material-ui/core/TextField';
 import FormGroup from '@material-ui/core/FormGroup';
-import Container from '@material-ui/core/Container'
+import Container from '@material-ui/core/Container';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel'
 import { makeStyles } from "@material-ui/core/styles";
 
 // styles for new participant form
@@ -21,11 +24,21 @@ const useStyles = makeStyles({
         borderColor: '#800000',
       }
     }
+  },
+  select: {
+    '&:after': {
+      borderColor: '#800000'
+    }   
   }
 })
 
 const NewParticipant = () => {
-  const classes = useStyles()
+  const classes = useStyles();
+
+  const [cause, setCause] = React.useState('')
+  const handleChange = (event) => {
+    setCause(event.target.value)
+  }
   return (
     <Fragment>
         <NavBar />
@@ -36,9 +49,21 @@ const NewParticipant = () => {
         <FormGroup className={classes.formGroup}>
             <TextField id="outlined-basic" fullWidth label="Name" variant="outlined" color="#8000000" className={classes.textField}/>
             <TextField id="outlined-basic" fullWidth label="Location" variant="outlined" className={classes.textField}/>
-            <TextField id="outlined-basic" fullWidth label="Your cause:" variant="outlined" className={classes.textField}/>
+            <InputLabel id="demo-simple-select-label" fullWidth className={classes.textField}>Your Cause</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={cause}
+          onChange={handleChange}
+          fullWidth
+          className={classes.textField, classes.select}
+        >
+          <MenuItem value={'Prostate Cancer'}>Prostate Cancer</MenuItem>
+          <MenuItem value={'Testicular Cancer'}>Testicular Cancer</MenuItem>
+          <MenuItem value={'Testicular Cancer'}>Men's mental health awareness</MenuItem>
+        </Select>
             <TextField id="outlined-basic" fullWidth label="Pledge" variant="outlined" className={classes.textField}/>
-            <TextField id="outlined-basic" fullWidth label="Why" variant="outlined" className={classes.textField}/>
+            <TextField id="outlined-basic" fullWidth label="Your Why" variant="outlined" className={classes.textField}/>
         </FormGroup>
       </Container>
     </Fragment>
